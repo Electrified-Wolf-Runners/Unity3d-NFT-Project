7af4044ba770968aa4a362704e69e37114aa14bf
shantanhunt <shantanusontakke015@gmail.com>
2021-11-06 21:39:11 +0530

    Added owners and tokenid in GamePool

--- Assets/Solidity Contracts/GamePool.sol
+++ Assets/Solidity Contracts/GamePool.sol
@@ -11,8 +11,18 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
 
-contract GamePool is ERC1155Receiver{
+contract GamePool is ERC1155Receiver, Ownable{
     IERC1155 nftcontract;
+    
+    // Mapping from obstacle_id to Owner address to token_id 
+    mapping(uint256 => mapping (address => uint256)) public _tokenid;
+    
+    // Mapping from obstacle_id to obstacles sold 
+    mapping (uint256 => uint256) public _sold;
+    
+    // Mapping from obstacle id to token ID to owner address
+    mapping(uint256 => mapping (uint256 => address)) public _owners;
+    
     constructor(address tokenAddress){
          nftcontract = IERC1155(tokenAddress);
     }
@@ -37,10 +47,18 @@ contract GamePool is ERC1155Receiver{
         return this.onERC1155BatchReceived.selector;
     }
     
-    function withdrawObstacles(uint256 obstacle_id) public view returns(uint256){
+    function withdrawObstacles(uint256 obstacle_id) public onlyOwner{
         uint256 obstacles = (nftcontract).balanceOf(address(this), obstacle_id);
-        // address owner = owner();
-        // this.safeTransferFrom(address(this), owner, obstacle_id, obstacles, "");
-        return obstacles;
+        address owner = owner();
+        (nftcontract).safeTransferFrom(address(this), owner, obstacle_id, obstacles, "");
+    }
+    
+    function buyObstacle(uint256 obstacle_id) public {
+        require((nftcontract).balanceOf(msg.sender, obstacle_id) < 2);
+        uint256 tokenid = _sold[obstacle_id];
+        _tokenid[obstacle_id][msg.sender] = tokenid; // token id
+        _owners[obstacle_id][tokenid] = msg.sender;  // to keep track of owners of each tokenid
+        _sold[obstacle_id] += 1;
+        (nftcontract).safeTransferFrom(address(this), msg.sender, obstacle_id, 1, "");
     }
 }
