5bb38440f4dea0a55ada81fa0e44b0fb7178e33f
shantanhunt <shantanusontakke015@gmail.com>
2021-11-18 22:35:29 +0530

    Updated GamePool contract

--- Assets/Solidity Contracts/EWO_Nfts.sol
+++ Assets/Solidity Contracts/EWO_Nfts.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts v4.3.2 (token/ERC1155/presets/ERC1155PresetMinterPauser.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
--- Assets/Solidity Contracts/GamePool.sol
+++ Assets/Solidity Contracts/GamePool.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts v4.3.2 (token/ERC1155/presets/ERC1155PresetMinterPauser.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/utils/ERC1155Holder.sol";
@@ -10,9 +10,11 @@ import "./EWO_Nfts.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/IERC1155.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 
 contract GamePool is ERC1155Receiver, Ownable{
     IERC1155 nftcontract;
+    IERC20 token;
     
     // Mapping from obstacle_id to Owner address to token_id 
     mapping(uint256 => mapping (address => uint256)) public _tokenid;
@@ -23,8 +25,19 @@ contract GamePool is ERC1155Receiver, Ownable{
     // Mapping from obstacle id to token ID to owner address
     mapping(uint256 => mapping (uint256 => address)) public _owners;
     
-    constructor(address tokenAddress){
-         nftcontract = IERC1155(tokenAddress);
+    // Prices of each obstacle
+    uint256[] public prices = [1000000000000000000, 1000000000000000000, 1000000000000000000];
+    
+    // Game Entry fee in Ewo token
+    uint256 public fee;
+    
+    //Events
+    event updatedFee(uint256 fee, address owner);
+    
+    constructor(address obstacleNFTAddress, address tokenAddress){
+         nftcontract = IERC1155(obstacleNFTAddress);
+         token = IERC20(tokenAddress);
+         fee = 100;
     }
     
     function onERC1155Received(
@@ -53,12 +66,22 @@ contract GamePool is ERC1155Receiver, Ownable{
         (nftcontract).safeTransferFrom(address(this), owner, obstacle_id, obstacles, "");
     }
     
-    function buyObstacle(uint256 obstacle_id) public {
-        require((nftcontract).balanceOf(msg.sender, obstacle_id) < 2);
+    function buyObstacle(uint8 obstacle_id) public payable {      //can remove payable
+        require(obstacle_id < 3, "obstacle_id does not exist");
+        require((nftcontract).balanceOf(msg.sender, obstacle_id) < 2, "You cannot buy more NFTs");
+        uint256 price = prices[obstacle_id];
+        require((token).transferFrom(msg.sender, address(this), price));
         uint256 tokenid = _sold[obstacle_id];
         _tokenid[obstacle_id][msg.sender] = tokenid; // token id
         _owners[obstacle_id][tokenid] = msg.sender;  // to keep track of owners of each tokenid
         _sold[obstacle_id] += 1;
         (nftcontract).safeTransferFrom(address(this), msg.sender, obstacle_id, 1, "");
     }
+    
+    function changeFee(uint256 _newfee) external onlyOwner{
+        fee = _newfee;
+        emit updatedFee(fee, msg.sender);
+    } 
+    
+    
 }
